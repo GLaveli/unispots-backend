2ef249617013438409b1ab8c945ebd44f6d06c70
Glaveli <g.laveli.p@gmail.com>
2020-01-27 23:17:51 -0300

    adicionado upload de imagens

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,5 +1,21 @@
+const Spot = require('../models/Spot');
+
 module.exports = {
     async store(req, res) {
-        return res.json({ ok: true })
+        const { filename } = req.file;
+        const { cellPhone, rooms, contract, contractTime, valor } = req.body;
+        const { user_id } = req.headers;
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            cellPhone,
+            rooms: rooms.split(',').map(room => room.trim()),
+            contract,
+            contractTime,
+            valor
+        });
+
+        return res.json(spot)
     }
 }
