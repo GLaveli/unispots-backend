1ca01059f85751c8b8045dae6f1c109f6eba75e0
Glaveli <g.laveli.p@gmail.com>
2020-02-03 09:51:05 -0300

    iniciado coleta de formulario /new

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -11,7 +11,7 @@ const SpotSchema = new mongoose.Schema({
     valor: Number,
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
     }
 }, {
     toJSON: {
