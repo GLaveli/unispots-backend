f932f70fcb1026b8e29e3ae224daa7410a54d589
glaveli <g.laveli.p@gmail.com>
2020-05-05 13:45:26 -0300

    .env removed

--- src/server.js
+++ src/server.js
@@ -12,9 +12,13 @@ mongoose.connect('mongodb+srv://NetUser:System_rw@kitnet-x03i0.mongodb.net/kitne
     useUnifiedTopology: true
 });
 
+const PORT = 3333;
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3000);
+app.listen(process.env.PORT || PORT, () => {
+    console.log('Unispot is running on PORT:', PORT);
+});
